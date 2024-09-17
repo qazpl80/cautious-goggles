d236088422772e910ff895057f23370c73c7152c
qazpl80 <76925476+qazpl80@users.noreply.github.com>
2024-09-17 13:52:50 +0800

    Job search in multiple pages
    User can input pages desired to extract jobs listing from

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import csv
 
 
 
-def find_job(position,location,user_skills):
+def find_job(position,location,user_skills,page_number):
     header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
     "Accept-Encoding": "gzip, deflate, br",
@@ -14,29 +14,36 @@ def find_job(position,location,user_skills):
     "Connection": "keep-alive",
     "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6"
 }
-    html_text = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=%22{position}%22&txtLocation={location}', headers=header).text
-    soup = BeautifulSoup(html_text, 'html.parser')
-    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    i=1
     info = []
-    for index, job in enumerate(jobs):
-        indiv = []
-        title = ''
-        company_name = ''
-        skill = job.find('span',class_='srp-skills').text.replace('\n','').replace('  ','').replace('&',' & ')
-        for user_skill in user_skills:
-            if user_skill in skill:
-                title_url = job.header.h2.a['href']
-                title_html = requests.get(str(title_url)).text
-                title_soup = BeautifulSoup(title_html, 'html.parser')
-                title = title_soup.find_all('h1', class_='jd-job-title')[0].text.replace('\n','').replace('\t','').replace('  ','').replace('"','')
-                indiv.append(title)
-                company_name = job.find('h3', class_='joblist-comp-name').text.replace('\n','').replace('(More Jobs)','').replace('  ','')
-                indiv.append(company_name)
-                indiv.append(skill)
-                info.append(indiv)
-            else:
-                continue
+    while i <= page_number:
+        html_text = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=%22{position}%22&txtLocation={location}&pDate=I&sequence=%22{i}&startPage=1', headers=header).text
+        #html_text2 = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=%22{position}%22&txtLocation={location}&pDate=I&sequence=%22{i}&startPage=1', headers=header)
+        #print(html_text2.status_code)
+        soup = BeautifulSoup(html_text, 'html.parser')
+        jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+        for index, job in enumerate(jobs):
+            indiv = []
+            title = ''
+            company_name = ''
+            skill = job.find('span',class_='srp-skills').text.replace('\n','').replace('  ','').replace('&',' & ')
+            for user_skill in user_skills:
+                if user_skill in skill:
+                    title_url = job.header.h2.a['href']
+                    title_html = requests.get(str(title_url)).text
+                    title_soup = BeautifulSoup(title_html, 'html.parser')
+                    title = title_soup.find_all('h1', class_='jd-job-title')[0].text.replace('\n','').replace('\t','').replace('  ','').replace('"','')
+                    indiv.append(title)
+                    company_name = job.find('h3', class_='joblist-comp-name').text.replace('\n','').replace('(More Jobs)','').replace('  ','')
+                    indiv.append(company_name)
+                    indiv.append(skill)
+                    info.append(indiv)
+                else:
+                    continue
+        i+=1
+    print(info)
     return info
+    
             
 def save_to_csv(info):
     with open('jobs.csv','w', newline='') as jf:
@@ -49,5 +56,6 @@ if __name__ == "__main__":
     position = input("Enter position/job: ")
     location = input("Enter location: ")
     user_skills = input("Enter your skills: ").split(',')
-    jobs = find_job(position,location,user_skills)
+    page_number = int(input("Enter number of pages to search: "))
+    jobs = find_job(position,location,user_skills,page_number)
     save_to_csv(jobs)
